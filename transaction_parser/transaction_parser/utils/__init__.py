f6ae156d47f7695d0af599384fc666846b9f02f4
Karm Soni <karmdsoni8159@gmail.com>
2025-10-03 18:28:44 +0530

    fix: add nosemgrep comment to database commit line

--- transaction_parser/transaction_parser/utils/__init__.py
+++ transaction_parser/transaction_parser/utils/__init__.py
@@ -44,7 +44,7 @@ def execute_in_new_transaction(fn):
             frappe.connect(set_admin_as_user=False)
             return fn(*args, **kwargs)
         finally:
-            frappe.db.commit()
+            frappe.db.commit()  # nosemgrep
             frappe.db.close()
             frappe.local.db = _db
 
